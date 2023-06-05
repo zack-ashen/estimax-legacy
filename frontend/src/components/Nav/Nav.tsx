64f40ac264c078ac66e681a42199e27657c24478
Zack Ashen <zachary.h.a@gmail.com>
2023-06-05 16:43:52 -0400

    start working on nav and add modules and scss

--- frontend/src/components/Nav/Nav.tsx
+++ frontend/src/components/Nav/Nav.tsx
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
+import { useAuth } from "../../contexts/AuthContext";
+
+import styles from "./Nav.module.css";
 
 interface NavProps {
     children: React.ReactElement<{}>;
+    landing: boolean;
 }
 
 
-function Nav({ children }: NavProps) {
-    
+function Nav({ children, landing=false }: NavProps) {
+    const auth = useAuth();
+    const [navType, setNavType] = useState(landingNav)
+
+    useEffect(() => {
+        if (!auth) {
+
+        } else {
+
+        }
+
+    }, [auth])
 
     return (
         <nav>
@@ -20,10 +34,70 @@ function Nav({ children }: NavProps) {
                 <ul>
 
                 </ul>
+                <div></div>
+                <div>
+
+                </div>
             </div>
         </nav>
     );
-
 }
+
+const contractorNav : NavLinks = {
+    links: [
+        {
+            text: "How It Works",
+            url: "#howitworks"
+        },
+
+    ],
+    buttons: [
+        <button value="hi">Sign Up</button>
+    ]
+}
+
+const homeownerNav : NavLinks = {
+    links: [
+        {
+            text: "How It Works",
+            url: "#howitworks"
+        },
+
+    ],
+    buttons: [
+        <button value="hi">Sign Up</button>
+    ]
+}
+
+const landingNav : NavLinks = {
+    links: [
+        {
+            text: "How It Works",
+            url: "#howitworks"
+        },
+        {
+            text: "Features",
+            url: "#features"
+        },
+        {
+            text: "FAQ",
+            url: "#faq"
+        }
+    ],
+    buttons: [
+        <button value="hi">Sign Up</button>
+    ]
+}
+
+interface Link {
+    text: string;
+    url: string;
+}
+
+interface NavLinks {
+    links: Link[];
+    buttons: JSX.Element[];
+}
+
 
 export default Nav;
